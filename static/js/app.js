478d2f0635a8da43caf232be551a965b4954843b
allison713 <allison.713@gmail.com>
2023-04-04 18:27:10 -0500

    began the drop down menu

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,30 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
+// Call updatePlotly() when a change takes place to the DOM
+d3.selectAll("#selDataset").on("change", updatePlotly);
+
+// This function is called when a dropdown menu item is selected
+function updatePlotly() {
+    // Use D3 to select the dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    let dataset = dropdownMenu.property("value");
+  
+    if (dataset === 'dataset1') {
+      x = [1, 2, 3, 4, 5];
+      y = [1, 2, 4, 8, 16];
+    }
+  
+    else if (dataset === 'dataset2') {
+      x = [10, 20, 30, 40, 50];
+      y = [1, 10, 100, 1000, 10000];
+    }
+  
+    // Note the extra brackets around 'x' and 'y'
+    Plotly.restyle("plot", "x", [x]);
+    Plotly.restyle("plot", "y", [y]);
+  }
+
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
     console.log(data);
