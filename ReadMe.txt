e309010fc539a5e5b999b1277b796ed84f7447e2
allison713 <allison.713@gmail.com>
2023-04-06 15:48:56 -0500

    got plots, but need to fix y-values

--- ReadMe.txt
+++ ReadMe.txt
@@ -4,3 +4,11 @@
 One name was able to be added with no problems, so the issue was with my loop.
 
 Fixed the problem by changing the length from data.length to data.names.length to specify that I want the loop to run until it runs out of names.
+
+
+*Sources*
+ - UTA class assignment 14-Interactive-Visualizations > 3 > Activities > 09-Ins_Dropdown_Events
+Sourced to assist in editing drop down menus and triggering a change.
+
+ - UTA class assignment 14-Interactive-Visualizations > 2 > Activities > 09-Stu_Top_Ten_Greek_Gods
+Sourced to assist in creating horizontal bar charts.
--- static/js/app.js
+++ static/js/app.js
@@ -3,16 +3,6 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 //Add a space for the graph on the webpage
 d3.select("body").append("div").attr("id","plot");
 
-//Start with a graph pre-loaded
-  //**********CHANGE THIS GRAPH*******/
-function init() {
-  data = [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }];
-
-  Plotly.newPlot("plot", data);
-};
-
 //Testing to see if I can add something to the dropdown menu
 // d3.json(url).then(function(importedData){
 //   let data = importedData
@@ -21,39 +11,64 @@ function init() {
 
 // Fetch the JSON data and add names to drop down list.
 d3.json(url).then(function(importedData) {
-  let data = importedData
+  let data = importedData;
+
+
+  //*********Find a way to turn the y-values into a list so they plot next to each other* */
+  //Start with a graph pre-loaded
+  function init() {
+    let trace = {
+      x: data.samples[0].sample_values.slice(0,11),
+      y: data.samples[0].otu_ids.slice(0,11).map(String),
+      text: data.samples[0].otu_ids.slice(0,11).map(String),
+      type: "bar",
+      orientation: "h"
+    };
+
+    let traceData = [trace];
+
+    let layout = {
+      title: `OTU for ${data.samples[0].id}`,
+    }
+
+    Plotly.newPlot("plot", traceData, layout);
+  };
+
+
+
+
+  console.log(data);
 
   //Add name IDs to the dropdown menu list
   for (let i=0;i<data.names.length;i++){
     d3.select("#selDataset").append("option").property("value",data.names[i]).text(data.names[i]);
   };
+
+  //Trigger to update the graph
+  d3.selectAll("#selDataset").on("change", updatePlotly);
+
+  //Function to change graph when dropdown is selected
+  function updatePlotly() {
+    // Use D3 to select the dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+
+    // Assign the value of the dropdown menu option to a variable
+    let name = dropdownMenu.property("value");
+
+    if (name === 'dataset1') {
+      x = [1, 2, 3, 4, 5];
+      y = [1, 2, 4, 8, 16];
+    }
+
+    else if (name === 'dataset2') {
+      x = [10, 20, 30, 40, 50];
+      y = [1, 10, 100, 1000, 10000];
+    }
+
+    // Note the extra brackets around 'x' and 'y'
+    Plotly.restyle("plot", "x", [x]);
+    Plotly.restyle("plot", "y", [y]);
+  };
+  init();
 });
 
-//Trigger to update the graph
-d3.selectAll("#selDataset").on("change", updatePlotly);
-
-//Function to change graph when dropdown is selected
-function updatePlotly() {
-  // Use D3 to select the dropdown menu
-  let dropdownMenu = d3.select("#selDataset");
-
-  // Assign the value of the dropdown menu option to a variable
-  let name = dropdownMenu.property("value");
-
-  if (name === 'dataset1') {
-    x = [1, 2, 3, 4, 5];
-    y = [1, 2, 4, 8, 16];
-  }
-
-  else if (name === 'dataset2') {
-    x = [10, 20, 30, 40, 50];
-    y = [1, 10, 100, 1000, 10000];
-  }
-
-  // Note the extra brackets around 'x' and 'y'
-  Plotly.restyle("plot", "x", [x]);
-  Plotly.restyle("plot", "y", [y]);
-};
-
-
-init();
