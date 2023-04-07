ed90a6996a876756be0f2521b74b7a4c6b8addd3
allison713 <allison.713@gmail.com>
2023-04-07 00:05:00 -0500

    got both graphs working.

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,8 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-//Add a space for the graph on the webpage
-d3.select("body").append("div").attr("id","plot");
-
-//Testing to see if I can add something to the dropdown menu
-// d3.json(url).then(function(importedData){
-//   let data = importedData
-//   d3.select("#selDataset").append("option").property("value",data.names[1]).text(data.names[1]);
-// })
+//Add a space for the graphs on the webpage
+d3.select("body").append("div").attr("id","bar_plot");
+d3.select("body").append("div").attr("id","bubble_chart");
 
 // Fetch the JSON data and add names to drop down list.
 d3.json(url).then(function(importedData) {
@@ -31,10 +26,35 @@ d3.json(url).then(function(importedData) {
       title: `Top 10 OTUs for ${data.samples[0].id}`,
     }
 
-    Plotly.newPlot("plot", traceData, layout);
+    Plotly.newPlot("bar_plot", traceData, layout);
   };
 
-  console.log(data);
+  //Start with a bubble graph pre-loaded
+  function init2() {
+    var otu_id = data.samples[0].otu_ids.map(x => x);
+    var sample_values = data.samples[0].sample_values.map(y => y);
+    var otu_label = data.samples[0].otu_labels.map(z => z);
+
+    let trace = {
+      x: otu_id,
+      y: sample_values,
+      mode: 'markers',
+      text: otu_label,
+      marker: {
+        color: otu_id,
+        colorscale: 'Portland',
+        size: sample_values
+      }
+    };
+
+    let traceData = [trace];
+
+    let layout = {
+      title: `OTUs by sample size for ${data.samples[0].id}`
+    }
+
+    Plotly.newPlot("bubble_chart", traceData, layout);
+  };
 
   //Add name IDs to the dropdown menu list
   for (let i=0;i<data.names.length;i++){
@@ -42,7 +62,10 @@ d3.json(url).then(function(importedData) {
   };
 
   //Trigger to update the graph
-  d3.selectAll("#selDataset").on("change", updatePlotly);
+  d3.selectAll("#selDataset").on("change", function() {
+    updatePlotly();
+    updatePlotly2();
+  });
 
   //Function to change graph when dropdown is selected
   function updatePlotly() {
@@ -56,9 +79,9 @@ d3.json(url).then(function(importedData) {
         var xChoice = data.samples[i].sample_values.slice(0,11);
         var yChoice = data.samples[i].otu_ids.slice(0,11).map(id => `OTU ${id}`);
         var textChoice = data.samples[i].otu_labels.slice(0,11).map(label => label);
-        var titles = data.samples[i].id
+        var title = data.samples[i].id;
       }
-
+      //update the graph
       update = {
         x: xChoice,
         y: yChoice,
@@ -69,15 +92,49 @@ d3.json(url).then(function(importedData) {
     }
 
     let trace = [update];
-
     let layout = {
-      title: `Top 10 OTUs for ${titles}`,
+      title: `Top 10 OTUs for ${title}`,
     }
-
-    Plotly.newPlot("plot", trace, layout);
+    //new graph
+    Plotly.newPlot("bar_plot", trace, layout);
   };
 
+  //Function to change graph when dropdown is selected
+  function updatePlotly2() {
+    // Use D3 to select the dropdown menu
+    let dropdownMenu2 = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    let name2 = dropdownMenu2.property("value");
+    // Loop to find a match to the new dropdown value.
+    for (let i=0;i<data.names.length;i++){
+      if (data.samples[i].id == name2) {
+        var xChoice2 = data.samples[i].otu_ids.map(x => x);
+        var yChoice2 = data.samples[i].sample_values.map(y => y);
+        var textChoice2 = data.samples[i].otu_labels.map(z => z);
+        var title2 = data.samples[i].id;
+      }
+      //update the graph
+      let update2 = {
+        x: xChoice2,
+        y: yChoice2,
+        mode: 'markers',
+        text: textChoice2,
+        marker: {
+          color: xChoice2,
+          colorscale: 'Portland',
+          size: yChoice2
+        }
+      };
+
+    let trace2 = [update2];
+    let layout2 = {
+      title: `OTUs by sample size for ${title2}`
+    }
+    //new graph
+    Plotly.newPlot("bubble_chart", trace2, layout2);
+  }};
 
   init();
+  init2();
 });
 
